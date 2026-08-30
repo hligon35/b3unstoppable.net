5f28e3b292602c1f4506d045e2bc22bb333c24d0
Harold Ligon <157246297+hligon35@users.noreply.github.com>
2026-08-30 01:38:47 -0400

    Fix admin subscribers imports

--- src/pages/admin/api/subscribers.ts
+++ src/pages/admin/api/subscribers.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-import { isAuthenticatedRequest } from '../../lib/adminAuth';
-import { deleteSubscriber, getSubscribers, insertSubscriber } from '../../lib/db';
+import { isAuthenticatedRequest } from '@/lib/adminAuth';
+import { deleteSubscriber, getSubscribers, insertSubscriber } from '@/lib/db';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
