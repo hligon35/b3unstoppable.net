68e97d34a73a7130d2da9abfcdf99ac37806eb9a
Harold Ligon <157246297+hligon35@users.noreply.github.com>
2026-08-30 01:33:21 -0400

    Fix admin analytics lib import paths

--- src/pages/admin/api/analytics.ts
+++ src/pages/admin/api/analytics.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-import { getAdminRole, isAuthenticatedRequest } from '../../lib/adminAuth';
-import { getAnalytics, getDeviceTypes, getTopBrowsers, getTopReferrers, getTotalViews, insertPageView } from '../../lib/db';
+import { getAdminRole, isAuthenticatedRequest } from '../../../lib/adminAuth';
+import { getAnalytics, getDeviceTypes, getTopBrowsers, getTopReferrers, getTotalViews, insertPageView } from '../../../lib/db';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
